0cf9c23057edcf18050ccc2a61cfbade0142b1b7
SCOTT\dogle <dogle@scottlogic.co.uk>
2016-11-15 15:02:03 +0000

    client setup

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ import {
 } from './src/server';
 
 export const store = makeStore();
-startServer();
+startServer(store);
+
+store.dispatch({
+    type: 'SET_ENTRIES',
+    entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
