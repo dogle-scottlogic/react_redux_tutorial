5182491cf40f804546597bc7d10255d8970192bd
SCOTT\dogle <dogle@scottlogic.co.uk>
2016-11-15 11:11:13 +0000

    add logic and tests for casting a vote

--- src/core.js
+++ src/core.js
@@ -1,11 +1,14 @@
 import {
-    List, Map
+    List,
+    Map
 } from 'immutable';
 
+// Takes a state and a list of entries and updates the state with a list 'entries'
 export function setEntries(state, entries) {
     return state.set('entries', List(entries));
 }
 
+// Next is called when voting is started. Takes the next two values from the list and adds them to the vote pair
 export function next(state) {
     const entries = state.get('entries');
     return state.merge({
@@ -14,4 +17,12 @@ export function next(state) {
         }),
         entries: entries.skip(2)
     });
+}
+
+// Takes an entry and adds a vote to it (sets tally to 0 first if it does not exist)
+export function vote(state, entry) {
+    return state.updateIn(['vote', 'tally', entry],
+        0,
+        tally => tally + 1
+    );
 }
