1186e6359b5bb31a9fa04bf75f4d8ab655ee2a06
SCOTT\dogle <dogle@scottlogic.co.uk>
2016-11-16 11:58:44 +0000

    add reducer logic for resetting the has voted flag

--- voting-client/src/reducer.js
+++ voting-client/src/reducer.js
@@ -1,4 +1,5 @@
 import {
+    List,
     Map
 } from 'immutable';
 
@@ -6,11 +7,31 @@ function setState(state, newState) {
     return state.merge(newState);
 }
 
+function vote(state, entry) {
+    const currentPair = state.getIn(['vote', 'pair']);
+    if (currentPair && currentPair.includes(entry)) {
+        return state.set('hasVoted', entry);
+    } else {
+        return state;
+    }
+}
+
+function resetVote(state) {
+    const hasVoted = state.get('hasVoted');
+    const currentPair = state.getIn(['vote', 'pair'], List());
+    if (hasVoted && !currentPair.includes(hasVoted)) {
+        return state.remove('hasVoted');
+    } else {
+        return state;
+    }
+}
+
 export default function(state = Map(), action) {
     switch (action.type) {
         case 'SET_STATE':
-            return setState(state, action.state);
-            break;
+            return resetVote(setState(state, action.state));
+        case 'VOTE':
+            return vote(state, action.entry);
         default:
             return state;
     }
