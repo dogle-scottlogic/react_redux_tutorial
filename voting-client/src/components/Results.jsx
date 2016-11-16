366d65139b91036e2f97fac3f1fa62298e2ac0ea
SCOTT\dogle <dogle@scottlogic.co.uk>
2016-11-16 11:13:55 +0000

    pair Results to redux state

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import Winner from './Winner';
 import Tally from './Tally';
 
-export default React.createClass({
+export const Results = React.createClass({
     render: function() {
         return <div>
             {this.props.winner
@@ -12,3 +13,13 @@ export default React.createClass({
         </div>;
     }
 });
+
+function mapStateToProps(state) {
+    return {
+        pair: state.getIn(['vote', 'pair']),
+        tally: state.getIn(['vote', 'tally']),
+        winner: state.get('winner')
+    }
+}
+
+export const ResultsContainer = connect(mapStateToProps)(Results);
